3968856760965da24a39fcfd56ca654ce859d5e4
Aiden Melendez <aidenjyo@gmail.com>
2019-12-09 09:00:10 -0500

    Initial Commit

---
+++ Week2WeatherBoy/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Week2WeatherBoy
+//
+//  Created by Consultant on 12/9/19.
+//  Copyright © 2019 MAC. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
